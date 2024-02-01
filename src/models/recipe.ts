597f28f4e262727f7ea9de51101f0afe5f6959ef
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-02-01 10:20:44 -0600

    Recipe Model

---
+++ src/models/recipe.ts
@@ -0,0 +1,18 @@
+export interface IRecipe 
+{
+    id:string,
+    title:string,
+    publisher:string,
+    image_url:string,
+    source_url:string,
+    servings:number,
+    cooking_time:number,
+    ingredients: IIngredient[]
+}
+
+export interface IIngredient
+{
+    description:string,
+    unit:string,
+    quantity:number
+}
