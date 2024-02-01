31a03706b9a03ed0136122572c0a359b8615b043
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-02-01 10:56:25 -0600

    Active ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css'
 import Header from './components/Header';
@@ -7,19 +7,33 @@ import Search from './components/Search';
 
 const queryClient = new QueryClient ();
 
+type AppContextType = {
+  activeId: string,
+  setActiveId: (val:string) => void
+}
+
+const AppContext = createContext<AppContextType> ({
+  activeId: '',
+  setActiveId: () => {}
+})
 
 function App() {
 
   const [searchStr, setSearchStr] = useState<string>('');
+  const [activeId, setActiveId] = useState<string>('');
 
   return (
+    <AppContext.Provider value={{activeId, setActiveId}}>
     <QueryClientProvider client={queryClient}>
       <div className='container'>
         <Header setSearchStr={setSearchStr}/>
         <Search searchStr={searchStr}/>
       </div>
     </QueryClientProvider>
+    </AppContext.Provider>
   )
 }
 
 export default App
+
+export { AppContext}
