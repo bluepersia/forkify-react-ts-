59613fce23cb31c2cf6a25310c5e98870dd86b99
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-02-01 10:47:35 -0600

    Search

---
+++ src/components/Header.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react"
+
+export default function Header ({setSearchStr}:{setSearchStr:(name:string) => void}) : JSX.Element
+{
+    const [name, setName] = useState<string>('');
+
+    function handleInputChange (e:React.ChangeEvent) : void
+    {
+        setName ((e.target as HTMLInputElement).value);
+    }
+
+    function handleFormSubmit (e:React.FormEvent) : void
+    {
+        e.preventDefault ();
+
+        setSearchStr (name);
+    }
+
+    return <header className="header">
+    <img src="src/img/logo.png" alt="Logo" className="header__logo" />
+    <form className="search" onSubmit={handleFormSubmit}>
+      <input
+        name="name"
+        value={name}
+        onChange={handleInputChange}
+        type="text"
+        className="search__field"
+        placeholder="Search over 1,000,000 recipes..."
+      />
+      <button className="btn search__btn">
+        <svg className="search__icon">
+          <use href="src/img/icons.svg#icon-search"></use>
+        </svg>
+        <span>Search</span>
+      </button>
+    </form>
+
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__item">
+          <button className="nav__btn nav__btn--add-recipe">
+            <svg className="nav__icon">
+              <use href="src/img/icons.svg#icon-edit"></use>
+            </svg>
+            <span>Add recipe</span>
+          </button>
+        </li>
+        <li className="nav__item">
+          <button className="nav__btn nav__btn--bookmarks">
+            <svg className="nav__icon">
+              <use href="src/img/icons.svg#icon-bookmark"></use>
+            </svg>
+            <span>Bookmarks</span>
+          </button>
+          <div className="bookmarks">
+            <ul className="bookmarks__list">
+              <div className="message">
+                <div>
+                  <svg>
+                    <use href="src/img/icons.svg#icon-smile"></use>
+                  </svg>
+                </div>
+                <p>
+                  No bookmarks yet. Find a nice recipe and bookmark it :)
+                </p>
+              </div>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </header>
+}
---
+++ src/components/Search.tsx
@@ -0,0 +1,61 @@
+import { useQuery } from "@tanstack/react-query"
+import { useEffect } from "react"
+import { IRecipe } from "../models/recipe";
+import { apiKey, baseURL } from "../utility";
+import Spinner from "./Spinner";
+import ErrorDisplay from "./ErrorDisplay";
+import RecipePreview from "./RecipePreview";
+
+export default function Search ({searchStr}:{searchStr:string}) : JSX.Element
+{
+    const {data:recipes, isLoading, error, refetch} = useQuery ({queryKey:['search'], queryFn: search});
+
+    async function search () : Promise<IRecipe[]>
+    {
+        const res = await fetch (`${baseURL}?search=${searchStr}&key=${apiKey}`);
+
+        if (!res.ok)
+          throw new Error ((await res.json()).message);
+
+        return (await res.json()).data.recipes;
+    }
+
+    useEffect (() =>
+    {
+      refetch ();
+    }, [searchStr])
+
+    return <div className="search-results">
+    <ul className="results">
+      {isLoading && <Spinner/>}
+      {error && <ErrorDisplay err={error}/>}
+      {recipes && recipes.map (recipe => <RecipePreview key={recipe.id} {...recipe}/>)}
+    </ul>
+
+    <div className="pagination">
+       <button className="btn--inline pagination__btn--prev">
+        <svg className="search__icon">
+          <use href="src/img/icons.svg#icon-arrow-left"></use>
+        </svg>
+        <span>Page 1</span>
+      </button>
+      <button className="btn--inline pagination__btn--next">
+        <span>Page 3</span>
+        <svg className="search__icon">
+          <use href="src/img/icons.svg#icon-arrow-right"></use>
+        </svg>
+      </button> 
+    </div>
+
+    <p className="copyright">
+      &copy; Copyright by
+      <a
+        className="twitter-link"
+        target="_blank"
+        href="https://twitter.com/jonasschmedtman"
+        >Jonas Schmedtmann</a
+      >. Use for learning or your portfolio. Don't use to teach. Don't claim
+      as your own.
+    </p>
+  </div>
+}
